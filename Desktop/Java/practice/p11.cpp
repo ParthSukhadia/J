d512b458a81df0fa427c587642fc1f7b3389fa16
ParthSukhadia <parthsukhadia383@gmail.com>
2024-08-21 18:09:42 +0530

    patterns

---
+++ Desktop/Java/practice/p11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ using namespace std;
+int main(){
+    int a;
+    cout<< "no. of row"<<endl;
+    cin>> a;
+
+    for (int i = 1;i <=a; ++i){
+        for (int j= 1;j <=a; ++j){
+            cout<<" ";
+        }
+    for (int k= 1;k <= 2 * i - 1; ++k)
+    {
+      cout<<"@"; 
+      }
+
+      cout<<endl ;
+    
+    }
+return 0 ;
+}
